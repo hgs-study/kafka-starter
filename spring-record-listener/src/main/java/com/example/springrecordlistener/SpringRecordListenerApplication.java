796a8eb45b7e40ee6ad5076fbf6b58ec062f8a94
hgs-study <hgstudy_@naver.com>
2022-07-30 01:35:17 +0900

    [spring-record-listener] create spring-record-listener

---
+++ spring-record-listener/src/main/java/com/example/springrecordlistener/SpringRecordListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrecordlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecordListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecordListenerApplication.class, args);
+	}
+
+}
