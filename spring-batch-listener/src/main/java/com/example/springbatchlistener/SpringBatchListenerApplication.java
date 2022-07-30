5fc0c378d1e8cf1da803a63708a315171633d013
hgs-study <hgstudy_@naver.com>
2022-07-30 16:14:32 +0900

    [spring-batch-listener] create spring-batch-listener

---
+++ spring-batch-listener/src/main/java/com/example/springbatchlistener/SpringBatchListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchListenerApplication.class, args);
+	}
+
+}
